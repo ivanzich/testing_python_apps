1dda55a2eab3e9955372e21a516ea9f2ae1dace5
Ivan Zich <ivanzich@gmail.com>
2020-07-23 17:27:01 -0700

    Added test for post.py

--- blog.py
+++ blog.py
@@ -1,3 +1,23 @@
+class Blog:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.posts = []
+
+    def __repr__(self):
+        return '{} by {} (0 posts)'. format(self.title, self.author)
+
+
+    def create_post(self, title, content):
+        pass
+
+    def json(self):
+        pass
+
+
+
+
+
 
 
 
