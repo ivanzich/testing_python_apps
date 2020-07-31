944feaeeca0c44720f0e819afb48535b467d5561
Ivan Zich <ivanzich@gmail.com>
2020-07-31 11:34:30 -0700

    Added integration tests

--- tests/unit/blog_test.py
+++ tests/unit/blog_test.py
@@ -14,8 +14,17 @@ class BlogTest(TestCase):
         b = Blog('Test', 'Test Author')
         b2 = Blog('My Day', 'Rolf')
 
-        self.assertEqual(b.__repr__(), 'Test by Test Author (0 posts)')
-        self.assertEqual(b2. __repr__(), 'My Day by Rolf (0 posts)')
+        self.assertEqual(b.__repr__(), 'Test by Test Author (1 post)')
+        self.assertEqual(b2. __repr__(), 'My Day by Rolf (2 posts)')
+
+
+    def test_create_post_in_blog(self):
+        b = Blog('Test', 'Test Author')
+        b.create_post('Test Post', 'Test Content')
+
+        self.assertEqual(len(b.posts), 1)
+        self.assertEqual(b.posts[0].title, 'Test Post')
+        self.assertEqual(b.posts[0].content, 'Test Content')
 
 
 
